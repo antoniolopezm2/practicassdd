53b2e42ccf2e555cd6859e25ae362ccdc8b132fe
antoniolopezm2 <antonio.lopezm2@um.es>
2024-02-12 12:46:04 +0100

    Texto para registrarse que redirecciona a página de registro

--- proyecto/frontend/app/forms.py
+++ proyecto/frontend/app/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, BooleanField, FileField)
-from wtforms.validators import InputRequired, Length, Email
+from wtforms.validators import InputRequired, Length, Email,EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[Email()])
     password = PasswordField('password', validators=[InputRequired()])
     remember_me = BooleanField('remember_me')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username',validators=[InputRequired(),Length(min=4,max=48)])
+    email = StringField('email', validators=[InputRequired(),Email()])
+    password = PasswordField('password', validators=[InputRequired()])
+    confirm_password = PasswordField('confirm password', validators=[InputRequired(), EqualTo('password')])
+    #Meter fechas , genero,etc
 
